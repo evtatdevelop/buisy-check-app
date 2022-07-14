a83c0d22bf2844251aa8d48ae5887494a8d2781a
Evgenii <evtatdevelop@gmail.com>
2022-07-14 10:57:01 +0700

    base funtional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
+import './rest.css';
+import Service from './services';
+
 function App() {
+  const [beepStatus, setBeepStatus] = useState('sleep');
+  const [event, setEvent] = useState('start');
+  const [prevs, setPrevs] = useState('start');
+
+  useEffect(() => {
+    
+    const getEvent = () => new Service().getevent().then(event => {
+      setEvent(event);
+      beep(event)
+    })
+
+    const beep = (newevent) => {
+      if ( prevs === newevent ) setBeepStatus('control');
+      else setBeepStatus('active');
+      setTimeout(() => setBeepStatus('sleep'), 1000)     
+      setPrevs(newevent)
+    }
+
+    let timer = setInterval(() => getEvent(), 5000);
+  
+    return () => clearTimeout(timer)
+  }, [event, prevs]);
+
+  const beeperClasses = `beepper ${beepStatus}`;
+
   return (
     <div className="App">
+      <h1>CorpSystems load control</h1>
+      <div className="dashboard">
+        <div className={beeperClasses}></div>
+        <p>{event.split(' ')[1]}</p>        
+      </div>
 
     </div>
   );
---
+++ src/services/index.js
@@ -0,0 +1,55 @@
+export default class Service {
+
+  getResource = async (url) => {
+    const res = await fetch(url, {
+      method: 'GET',
+      // headers: {
+      //   'Content-Type': 'application/json;charset=utf-8',
+      //   'API-Key': 'fL1XVQ5CeeyZ6sBcQlgthfoXeZDxqY'
+      // }
+    });
+    if (!res.ok) throw new Error(`Could non fetch ${url}. Status: ${res.status}`);
+    return await res.json();   
+  }
+
+
+  getevent = () => this.getResource(`https://request.sibgenco.local/services/?data=lastactivity&key=fL1XVQ5CeeyZ6sBcQlgthfoXeZDxqY`);
+
+  // postResource = async (url, data) => {
+  //   const res = await fetch(url, {
+  //     method: 'POST',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //       'API-Key': 'fL1XVQ5CeeyZ6sBcQlgthfoXeZDxqY'
+  //     }
+  //   });
+  //   if (!res.ok) throw new Error(`Could non fetch ${url}. Status: ${res.status}`);
+  //   return await res.json(); 
+  // }
+
+  // updateResource = async (url, data) => {
+  //   const res = await fetch(url, {
+  //     method: 'PATCH',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //       'API-Key': 'fL1XVQ5CeeyZ6sBcQlgthfoXeZDxqY'
+  //     }
+  //   });
+  //   if (!res.ok) throw new Error(`Could non fetch ${url}. Status: ${res.status}`);
+  //   return await res.json(); 
+  // }
+
+  // deleteResource = async (url) => {
+  //   const res = await fetch(url, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //       'API-Key': 'fL1XVQ5CeeyZ6sBcQlgthfoXeZDxqY'
+  //     }
+  //   });
+  //   if (!res.ok) throw new Error(`Could non fetch ${url}. Status: ${res.status}`);
+  //   return await res.json(); 
+  // }
+}
